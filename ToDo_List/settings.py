7e5bcc508cae21741e545af7bbcc01c0fd22ffbb
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-06-07 11:01:37 +0530

    error solved : collect static files

--- ToDo_List/settings.py
+++ ToDo_List/settings.py
@@ -122,10 +122,10 @@ LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
+    os.path.join(BASE_DIR, "static"),
 )
 
 # Default primary key field type
